c30c98707ed9284973824dbf81d0b8a0c30e870c
Wejdenchouaibi <wijdenchouaibi@gmail.com>
2026-02-23 10:56:28 +0100

    message

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,11 @@
+import { IsOptional, IsString } from "class-validator";
+
 export class AuthDto {
+    @IsString()
+    @IsOptional()
+
     email: string;
+     @IsString()
+    @IsOptional()
     password: string;
 }
